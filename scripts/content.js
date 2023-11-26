865ff81474ea826662bf94671c473d72a772f1f3
Simon de Villiers <simon.devilliers@octoenergy.com>
2023-11-26 02:24:33 +0000

    Just make them red instead of removing

--- scripts/content.js
+++ scripts/content.js
@@ -1,32 +1,69 @@
-const propertyLinks = document.getElementsByClassName("propertyCard-link property-card-updates");
-const searchResultsList = document.getElementsByClassName("l-searchResults").item(0).childNodes.item(0);
+function removeNode(node) {
+	node.parentNode.removeChild(node);
+}
 
-if (propertyLinks) {
-	for (let index = 0; index < propertyLinks.length; index++) {
-		var content = propertyLinks.item(index);
-		var link = "https://www.rightmove.co.uk" + content.getAttribute("href");
+function removeNodeByDataTestAttr(index) {
+	var node = document.querySelector(`[data-test="propertyCard-${index}"]`);
+	removeNode(node);
+}
 
-		fetch(link)
-			.then(function(response) {
-				return response.text()
-			})
-			.then(function(html) {
-				var parser = new DOMParser();
-				var doc = parser.parseFromString(html, "text/html");
+function colourNodeByDataTestAttr(index, tenure) {
+	var node = document.querySelector(`[data-test="propertyCard-${index}"]`);
+	node.style['color'] = '#ff0000';
+	node.querySelector('[data-test="property-description"]').childNodes.item(0).textContent = tenure;
+}
 
-				var dd = doc.getElementsByClassName('_1hV1kqpVceE9m-QrX_hWDN _2SpNNVW0fTYoFvPDmhKSt8 ')
-				for (let i = 0; i < dd.length; i++) {
-					var text = dd.item(i).textContent;
-					if (text == 'Leasehold' || text == 'Ask agent') {
-						var nodeToRemove = searchResultsList.childNodes.item(index);
-						console.log(nodeToRemove.textContent);
-						nodeToRemove.parentNode.removeChild(nodeToRemove);
+function removeLeaseholdProperties() {
+	const propertyLinks = document.getElementsByClassName("propertyCard-link property-card-updates");
+
+	var badNodeIndices = [];
+
+	if (propertyLinks) {
+		for (let index = 0; index < propertyLinks.length; index++) {
+			var content = propertyLinks.item(index);
+			var link = "https://www.rightmove.co.uk" + content.getAttribute("href");
+
+			fetch(link)
+				.then(function(response) {
+					return response.text()
+				})
+				.then(function(html) {
+					var parser = new DOMParser();
+					var doc = parser.parseFromString(html, "text/html");
+
+					var dd = doc.getElementsByClassName('_1hV1kqpVceE9m-QrX_hWDN _2SpNNVW0fTYoFvPDmhKSt8 ')
+					for (let i = 0; i < dd.length; i++) {
+						var text = dd.item(i).textContent;
+						if (text == 'Leasehold') {
+							if (!badNodeIndices.includes(index)) {
+								badNodeIndices.push(index);
+								colourNodeByDataTestAttr(index, "LEASEHOLD");
+							}
+						} else if (text == "Ask agent") {
+							if (!badNodeIndices.includes(index)) {
+								badNodeIndices.push(index);
+								colourNodeByDataTestAttr(index, "ASK AGENT");
+							}
+						}
 					}
-				}
-
-			})
-			.catch(function(err) {
-				console.log('Failed to fetch page: ', err);
-			});
+					var button = doc.querySelector('[aria-label="tenure LEASEHOLD, this link scrolls down to leasehold information section"]');
+					if (button) {
+						if (!badNodeIndices.includes(index)) {
+							badNodeIndices.push(index);
+							colourNodeByDataTestAttr(index);
+						}
+					}
+				})
+				.catch(function(err) {
+					console.log('Failed to fetch page: ', err);
+				});
+		}
+		console.log(badNodeIndices);
+		badNodeIndices.forEach((i) => {
+			console.log(i);
+			removeNodeByDataTestAttr(i);
+		})
 	}
 }
+
+removeLeaseholdProperties();
